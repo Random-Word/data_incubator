10315bd6483e59cd4128bbc9260ed7cfa3503238
Ross Story <ross.story@dal.ca>
2015-10-29 20:31:46 -0300

    Added more questions.

--- script/manhattan.py
+++ script/manhattan.py
@@ -23,17 +23,25 @@ def thresholder(f, threshold):
     x, y = f()
     return np.sqrt((x**2+y**2)) >= threshold
 
-def Q1(f, steps, threshold, iterations):
-    p = multiprocessing.Pool(multiprocessing.cpu_count)
-    f = functools.partial(f, functools.partial(simulate, steps))
-    results = list()
-    data_set = np.ndarray((iterations, steps, 2))
-    for s in range(steps-1):
-        data_set[:, s+1, :] = data_set[:, s, :] + np.vstack(
-                np.random.choice([-1, 0, 1, 0]),
-                np.random.choice([-1, 0, 1, 0]))
-    #result[:,:,:] = np.asarray(p.map(f, itertools.repeat(threshold, iterations)))
-    return (np.sum(result)/result.size)
+def euclidean(data):
+    return np.sqrt(data[:, 0]**2 + data[:, 1]**2)
+
+def at_least_at(data_set, step, distance):
+    return (np.sum(euclidean(data_set[:, step-1, :]) >= distance)) / data_set.size
+
+def at_least_by(data_set, step, distance):
+    passed = np.zeros((data_set.shape[0]), dtype=bool)
+    for s in range(step):
+        passed[euclidean(data_set[:, s, :]) >= distance] = 1
+    return np.sum(passed)/passed.size
+
+def across_the_iron_curtain(data_set, step):
+    east = np.zeros((data_set.shape[0]), dtype=bool)
+    west = np.zeros((data_set.shape[0]), dtype=bool)
+    for s in range(step):
+        east[data_set[:,s,0]>0] = True
+    west[np.logical_and(east, data_set[:,step-1,0]<0)] = True
+    return np.sum(west)/west.size
 
 def gen_data(iterations, steps):
     data_set = np.ndarray((iterations, steps, 2))
@@ -45,12 +53,32 @@ def gen_data(iterations, steps):
         stepsy[stepsx!=0]=0
         data_set[:, s+1, 0] = data_set[:, s, 0] + stepsx
         data_set[:, s+1, 1] = data_set[:, s, 1] + stepsy
+    return data_set
 
 
 #timed = functools.partial(Q1, thresholder, 10, 3, 10000)
-start = time.clock()
-print(gen_data(1000000, 60))
-total = time.clock()-start
-print(total)
+print("Generating data...")
+data_set = gen_data(1000000, 120)
+print(data_set[0,:,:])
+
+print("Q1:")
+print(at_least_at(data_set, 10, 3))
+
+print("Q2:")
+print(at_least_at(data_set, 60, 10))
+
+print("Q3:")
+print(at_least_by(data_set, 10, 5))
+
+print("Q4:")
+print(at_least_by(data_set, 60, 10))
+
+print("Q5:")
+print(across_the_iron_curtain(data_set, 10))
+
+print("Q6:")
+print(across_the_iron_curtain(data_set, 30))
+
+
 #print(Q1(thresholder, 10, 3, 1000000))
 
