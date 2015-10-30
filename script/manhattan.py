c4edabb75728a9ecca1029cde8fd7cc797b74779
Ubuntu <ubuntu@ip-172-31-11-85.us-west-1.compute.internal>
2015-10-30 16:44:04 +0000

    Added NYC script, improved manhattan.

--- script/manhattan.py
+++ script/manhattan.py
@@ -7,36 +7,24 @@ import itertools
 import functools
 import time
 
-"""def simulate(steps):
-    pos=np.zeros(2)
-    for i in range(steps):
-        pos += random.choice([(-1,0),(1,0),(0,-1),(0,1)])
-    return pos"""
-
-def simulate(steps):
-    to_return = np.zeroes((steps, 2))
-    for i in range(steps-1):
-        to_return[i+1,:] = to_return[i,:] + random.choice([(-1,0),(1,0),(0,-1),(0,1)])
-    return to_return
-
-def thresholder(f, threshold):
-    x, y = f()
-    return np.sqrt((x**2+y**2)) >= threshold
+MAX_SIZE = 10000000
+STEPS = 60
+THREADS = 15
 
 def euclidean(data):
     return np.sqrt((data[:, 0].astype(np.float64,copy=False)**2)+
-	(data[:, 1].astype(np.float64,copy=False)**2))
+            (data[:, 1].astype(np.float64,copy=False)**2))
 
 def at_least_at(data_set, step, distance):
-    return (np.sum(
+    total = np.sum(
         euclidean(data_set[:, step-1, :]) >= distance)
-        ) / data_set.shape[0]
+    return total, data_set.shape[0], total/data_set.shape[0]
 
 def at_least_by(data_set, step, distance):
     passed = np.zeros((data_set.shape[0]), dtype=bool)
     for s in range(step):
         passed[euclidean(data_set[:, s, :]) >= distance] = 1
-    return np.sum(passed)/passed.size
+    return np.sum(passed), passed.size, np.sum(passed)/passed.size
 
 def across_the_iron_curtain(data_set, step):
     east = np.zeros((data_set.shape[0]), dtype=bool)
@@ -44,7 +32,19 @@ def across_the_iron_curtain(data_set, step):
     for s in range(step):
         east[data_set[:,s,0]>0] = True
     west[np.logical_and(east, data_set[:,step-1,0]<0)] = True
-    return np.sum(west)/west.size
+    return np.sum(west), west.size, np.sum(west)/west.size
+
+def avg_moves_to_distance(data_set, distance):
+    not_reached_mark = np.ndarray((data_set.shape[0]), dtype = np.bool)
+    not_reached_mark[:] = True
+    steps_required = np.zeros((data_set.shape[0]), dtype = np.int8)
+    for s in range(data_set.shape[1]):
+        made_it = euclidean(data_set[:,s,:])>=distance
+        steps_required[np.logical_and(not_reached_mark, made_it)] = s
+        not_reached_mark[made_it] = False
+    steps_required = steps_required[steps_required!=0]
+    samples = steps_required.size
+    return steps_required, samples, np.mean(steps_required)
 
 def gen_data(iterations, steps):
     data_set = np.ndarray((iterations, steps, 2), dtype=np.int8)
@@ -58,28 +58,42 @@ def gen_data(iterations, steps):
         data_set[:, s+1, 1] = data_set[:, s, 1] + stepsy
     return data_set
 
+def gen_async(thread_count, pool):
+    to_return = list()
+    for i in range(thread_count):
+        to_return.append(pool.apply_async(gen_data,[MAX_SIZE,STEPS]))
+    return to_return
 
-#timed = functools.partial(Q1, thresholder, 10, 3, 10000)
-print("Generating data...")
-MAX_SIZE = 10000000
-data_set = gen_data(MAX_SIZE,60)
-data_set2 = gen_data(MAX_SIZE,60)
-data_set3 = gen_data(MAX_SIZE,60)
-data_set = np.vstack((data_set, data_set2, data_set3))
-print("Q1:")
-print(at_least_at(data_set, 10, 3))
 
-print("Q2:")
-print(at_least_at(data_set, 60, 10))
+if __name__ == '__main__':
+    print("Generating data...")
 
-print("Q3:")
-print(at_least_by(data_set, 10, 5))
+    pool = multiprocessing.Pool(multiprocessing.cpu_count())
 
-print("Q4:")
-print(at_least_by(data_set, 60, 10))
+    data_set = np.vstack([x.get() for x in gen_async(THREADS, pool)])
+    print("DS Shape:")
+    print(data_set.shape)
 
-print("Q5:")
-print(across_the_iron_curtain(data_set, 10))
+    print("Q1:")
+    print(at_least_at(data_set, 10, 3))
 
-print("Q6:")
-print(across_the_iron_curtain(data_set, 30))
+    print("Q2:")
+    print(at_least_at(data_set, 60, 10))
+
+    print("Q3:")
+    print(at_least_by(data_set, 10, 5))
+
+    print("Q4:")
+    print(at_least_by(data_set, 60, 10))
+
+    print("Q5:")
+    print(across_the_iron_curtain(data_set, 10))
+
+    print("Q6:")
+    print(across_the_iron_curtain(data_set, 30))
+
+    print("Q7:")
+    print(avg_moves_to_distance(data_set, 10))
+
+    #print("Q8:")
+    #print(avg_moves_to_distance(data_set, 60))
---
+++ script/nyc.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+
+DATA_PATH = '../data/rows.csv'
+R = 6371 #Radius of the Earth in km
+
+def lat_lon_dist(lat1, lon1, lat2, lon2):
+    """Use Haverton's equation to compute distance between
+    Lat/Long points in km."""
+    lat1 = np.radians(lat1)
+    lat2 = np.radians(lat2)
+    lon1 = np.radians(lon1)
+    lon2 = np.radians(lon2)
+    dlat = lat1-lat2
+    dlon = lon1-lon2
+    a = np.sin(dlat/2)**2 + np.cos(lat1)*np.cos(lat2) * np.sin(dlon)**2
+    c = 2*np.arctan2(np.sqrt(a), np.sqrt(1-a))
+    d = R*c
+    return d
+
+Q1_data = pd.read_csv(DATA_PATH, usecols=['Agency'])
+counts = Q1_data['Agency'].value_counts()
+print("Q1:")
+print(counts[1]/counts.sum())
+
+Q2_data = pd.read_csv(DATA_PATH, usecols=['Latitude', 'Longitude'])
+print("Q2:")
+print(Q2_data.quantile(0.9,axis=0)-Q2_data.quantile(0.1,axis=0))
+
+print("Q3:")
+mean_lat = Q2_data['Latitude'].mean()
+lat_std = Q2_data['Latitude'].std()
+mean_lon = Q2_data['Longitude'].mean()
+lon_std = Q2_data['Longitude'].std()
+
+lat_dist = lat_lon_dist(mean_lat+lat_std, mean_lon, mean_lat-lat_std,
+        mean_lon)/2.0
+lon_dist = lat_lon_dist(mean_lat, mean_lon+lon_std, mean_lat,
+        mean_lon-lon_std)/2.0
+
+print(np.pi*lon_dist*lat_dist)
+
+Q3_data = pd.read_csv(DATA_PATH,  usecols=['Created Date'])
+print(Q3_data.describe())
+dates = pd.to_datetime(Q3_data['Created Date'])
+print(dates.hour)
+print(dates.hour.value_counts())
